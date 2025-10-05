1559be74c7546526f46e7537ce22aabc7ebad9ef
Aleksey Kostyunin <kostunin-ag@mail.ru>
2025-10-05 15:35:52 +0500

    Обновление от 051025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.alekseykostyunin.enot"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
